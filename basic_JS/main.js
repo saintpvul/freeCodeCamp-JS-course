641dcc3cb843dbb28cf96b9d243ad78d0b92cdf4
saintpvul <saintpvul@gmail.com>
2022-07-20 00:07:47 +0600

    feat: complete ex12

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,4 +1,4 @@
-// Add Two Numbers with JavaScript
-// Change the 0 so that sum will equal 20.
+// Subtract One Number from Another with JavaScript
+// Change the 0 so the difference is 12.
 
-const sum = 10 + 10;
+const difference = 45 - 33;
