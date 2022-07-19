ad9371dddccb0dd65c0722986f603ebb61eaf70a
saintpvul <saintpvul@gmail.com>
2022-07-20 00:03:56 +0600

    feat: complete ex8

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,14 +1,5 @@
-// Understanding Case Sensitivity in Variables
-// Modify the existing declarations and assignments so their names use camelCase.
+// Explore Differences Between the var and let Keywords
+// Update the code so it only uses the let keyword.
 
-// Do not create any new variables.
-
-// Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
-
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+let catName = "Oliver";
+let catSound = "Meow!";
