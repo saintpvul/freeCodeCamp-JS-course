0f249a9d0d577a5a7285c4764313340298e718e3
saintpvul <saintpvul@gmail.com>
2022-07-19 23:57:43 +0600

    feat: complete ex4

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,4 +1,8 @@
-// Storing Values with the Assignment Operator
-// Assign the value 7 to variable a.
+// Assigning the Value of One Variable to Another
+// Assign the contents of a to variable b.
+// Setup
 var a;
 a = 7;
+var b;
+b = a;
+// Only change code below this line
