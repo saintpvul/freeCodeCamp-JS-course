0b723e89d2269b7d845f19909ea55cb1a99b1497
saintpvul <saintpvul@gmail.com>
2022-07-20 00:08:48 +0600

    feat: complete ex14

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,5 +1,5 @@
-// Multiply Two Numbers with JavaScript
+// Divide One Number by Another with JavaScript
 
-// Change the 0 so that product will equal 80.
+// Change the 0 so that the quotient is equal to 2.
 
-const product = 8 * 10;
+const quotient = 66 / 33;
