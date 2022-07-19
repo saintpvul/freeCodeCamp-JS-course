d4969c52be0d3ee40dacffec5d4e663dff98f31f
saintpvul <saintpvul@gmail.com>
2022-07-19 23:59:58 +0600

    feat: complete ex5

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,8 +1,3 @@
-// Assigning the Value of One Variable to Another
-// Assign the contents of a to variable b.
-// Setup
-var a;
-a = 7;
-var b;
-b = a;
-// Only change code below this line
+// Initializing Variables with the Assignment Operator
+// Define a variable a with var and initialize it to a value of 9.
+var a = 9;
