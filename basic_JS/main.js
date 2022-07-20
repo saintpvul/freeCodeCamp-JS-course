e2864158f5e7be0c6e30b79c8833dc4178c93cae
saintpvul <saintpvul@gmail.com>
2022-07-21 01:16:41 +0600

    feat: complete ex45

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,10 +1,10 @@
-// Manipulate Arrays With push()
-// Push ["dog", 3] onto the end of the myArray variable.
+// Manipulate Arrays With pop()
+// Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
 
 // Setup
 const myArray = [
   ["John", 23],
   ["cat", 2],
 ];
-myArray.push(["dog", 3]);
+const removedFromMyArray = myArray.pop();
 // Only change code below this line
