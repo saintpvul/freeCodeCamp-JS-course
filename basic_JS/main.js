e097ee552db0ef6770338a781e3b28970702680d
saintpvul <saintpvul@gmail.com>
2022-07-21 01:24:08 +0600

    feat: complete ex49

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,14 +1,10 @@
-// Shopping List
-/* Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
-
-The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
-There should be at least 5 sub-arrays in the list.
+// Write Reusable JavaScript with Functions
+/* Create a function called reusableFunction which prints the string Hi World to the dev console.
+Call the function.
 */
 
-const myList = [
-  ["Banana", 2],
-  ["Mango", 7],
-  ["Avocado", 1],
-  ["Coconut", 5],
-  ["Melon", 4],
-];
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction();
