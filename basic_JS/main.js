1ba9e15ad781f3cc35285025e9f4db8d6b38f889
saintpvul <saintpvul@gmail.com>
2022-07-21 01:21:26 +0600

    feat: complete ex48

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,11 +1,14 @@
-// Manipulate Arrays With unshift()
-// Add ["Paul", 35] to the beginning of the myArray variable using unshift().
+// Shopping List
+/* Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
 
-// Setup
-const myArray = [
-  ["John", 23],
-  ["dog", 3],
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+There should be at least 5 sub-arrays in the list.
+*/
+
+const myList = [
+  ["Banana", 2],
+  ["Mango", 7],
+  ["Avocado", 1],
+  ["Coconut", 5],
+  ["Melon", 4],
 ];
-myArray.shift();
-myArray.unshift(["Paul", 35]);
-// Only change code below this line
