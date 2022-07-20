48724ed42e74510fcc6943f78e967d487645615b
saintpvul <saintpvul@gmail.com>
2022-07-21 01:06:14 +0600

    feat: complete ex37

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,8 +1,8 @@
-// Use Bracket Notation to Find the Last Character in a String
-// Use bracket notation to find the last character in the lastName variable.
+// Use Bracket Notation to Find the Nth-to-Last Character in a String
+// Use bracket notation to find the second-to-last character in the lastName string.
 
 // Setup
 const lastName = "Lovelace";
 
 // Only change code below this line
-const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
