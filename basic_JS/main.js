74edf18a92051f5a7562d6c76a3a6706ed9417d9
saintpvul <saintpvul@gmail.com>
2022-07-21 01:17:29 +0600

    feat: complete ex46

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,10 +1,10 @@
-// Manipulate Arrays With pop()
-// Use the .pop() function to remove the last item from myArray and assign the popped off value to a new variable, removedFromMyArray.
+// Manipulate Arrays With shift()
+// Use the .shift() function to remove the first item from myArray and assign the "shifted off" value to a new variable, removedFromMyArray.
 
 // Setup
 const myArray = [
   ["John", 23],
-  ["cat", 2],
+  ["dog", 3],
 ];
-const removedFromMyArray = myArray.pop();
+const removedFromMyArray = myArray.shift();
 // Only change code below this line
