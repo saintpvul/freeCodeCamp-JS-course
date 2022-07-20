669007406842c1cddedd20fb2ee66a18324f1de5
saintpvul <saintpvul@gmail.com>
2022-07-21 01:55:30 +0600

    feat: complete ex58

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,24 +1,14 @@
-// Stand in Line
+// Understanding Boolean Values
 /* 
 
-Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
 
-Add the number to the end of the array, then remove the first element of the array.
-
-The nextInLine function should then return the element that was removed.
 */
 
-function nextInLine(arr, item) {
+function welcomeToBooleans() {
   // Only change code below this line
-  arr.push(item);
-  return arr.shift(item);
+
+  return true; // Change this line
+
   // Only change code above this line
 }
-
-// Setup
-const testArr = [1, 2, 3, 4, 5];
-
-// Display code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
