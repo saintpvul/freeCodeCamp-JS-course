0c375ab30000f5f704a3c6ffe96f95aeecf1bf78
saintpvul <saintpvul@gmail.com>
2022-07-21 00:39:47 +0600

    feat: complete ex30

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,5 +1,5 @@
-// Concatenating Strings with the Plus Equals Operator
-// Build myStr over several lines by concatenating these two strings: This is the first sentence. and This is the second sentence. using the += operator. Use the += operator similar to how it is shown in the example and be sure to include a space between the two strings. Start by assigning the first string to myStr, then add on the second string.
+// Constructing Strings with Variables
+// Set myName to a string equal to your name and build myStr with myName between the strings My name is and and I am well!
 
-let myStr = "This is the first sentence. ";
-myStr += "This is the second sentence.";
+let myName = "Paul";
+let myStr = "my name is " + myName + " and I am well!";
