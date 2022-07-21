755ae0d8af0dc6bfcecf1bde841c94bd80317d2b
saintpvul <saintpvul@gmail.com>
2022-07-21 22:42:31 +0600

    feat: complete ex79

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,34 +1,30 @@
-// Multiple Identical Options in Switch Statements
+// Replacing If Else Chains with Switch
 
 /*
 
-Write a switch statement to set answer for the following ranges:
-1-3 - Low
-4-6 - Mid
-7-9 - High
-
-Note: You will need to have a case statement for each number in the range.
+Change the chained if/else if statements into a switch statement.
 
 */
 
-function sequentialSizes(val) {
+function chainToSwitch(val) {
   let answer = "";
   // Only change code below this line
+
   switch (val) {
-    case 1:
-    case 2:
-    case 3:
-      answer = "Low";
+    case "bob":
+      answer = "Marley";
       break;
-    case 4:
-    case 5:
-    case 6:
-      answer = "Mid";
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
       break;
     case 7:
-    case 8:
-    case 9:
-      answer = "High";
+      answer = "Ate Nine";
       break;
   }
 
@@ -36,4 +32,4 @@ function sequentialSizes(val) {
   return answer;
 }
 
-sequentialSizes(1);
+chainToSwitch(7);
