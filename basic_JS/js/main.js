fdde982826959cb4ad246f08643e52848ff5d323
saintpvul <saintpvul@gmail.com>
2022-07-22 01:44:00 +0600

    feat: complete ex88

--- basic_JS/js/main.js
+++ basic_JS/js/main.js
@@ -1,18 +1,15 @@
-// Updating Object Properties
+// Add New Properties to a JavaScript Objectf
 
 /*
 
-Update the myDog object's name property. Let's change her name from Coder to Happy Coder. You can use either dot or bracket notation.
+Add a bark property to myDog and set it to a dog sound, such as "woof". You may use either dot or bracket notation.
 
 */
 
-// Setup
 const myDog = {
-  name: "Coder",
+  name: "Happy Coder",
   legs: 4,
   tails: 1,
   friends: ["freeCodeCamp Campers"],
 };
-
-// Only change code below this line
-myDog.name = "Happy Coder";
+myDog.bark = "Woof";
