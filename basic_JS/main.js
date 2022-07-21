6d5d7f471de1e4db9f989f324013e2ee85fc370f
saintpvul <saintpvul@gmail.com>
2022-07-21 22:03:50 +0600

    feat: complete ex75

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,32 +1,51 @@
-// Chaining If Else Statements
+// Golf Code
 
 /* 
 
-Write chained if/else if statements to fulfill the following conditions:
+In the game of Golf, each hole has a par, meaning, the average number of strokes a golfer is expected to make in order to sink the ball in the hole to complete the play. Depending on how far above or below par your strokes are, there is a different nickname.
 
-num < 5 - return Tiny
-num < 10 - return Small
-num < 15 - return Medium
-num < 20 - return Large
-num >= 20 - return Huge
+Your function will be passed par and strokes arguments. Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest):
 
+Strokes	Return
+1	"Hole-in-one!"
+<= par - 2	"Eagle"
+par - 1	"Birdie"
+par	"Par"
+par + 1	"Bogey"
+par + 2	"Double Bogey"
+>= par + 3	"Go Home!"
+par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
 */
 
-function testSize(num) {
+const names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!",
+];
+
+function golfScore(par, strokes) {
   // Only change code below this line
-  if (num < 5) {
-    return "Tiny";
-  } else if (num < 10 && num >= 5) {
-    return "Small";
-  } else if (num < 15 && num >= 10) {
-    return "Medium";
-  } else if (num < 20 && num >= 15) {
-    return "Large";
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes == par - 1) {
+    return names[2];
+  } else if (strokes == par) {
+    return names[3];
+  } else if (strokes == par + 1) {
+    return names[4];
+  } else if (strokes == par + 2) {
+    return names[5];
   } else {
-    return "Huge";
+    return names[6];
   }
-  return "Change Me";
+
   // Only change code above this line
 }
 
-testSize(7);
+golfScore(5, 4);
