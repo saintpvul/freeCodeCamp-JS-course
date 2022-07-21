4cd1c29e9754ee783e0f7f8deff3c91bb654afbc
saintpvul <saintpvul@gmail.com>
2022-07-22 01:19:21 +0600

    feat: complete ex84

--- basic_JS/js/main.js
+++ basic_JS/js/main.js
@@ -1,19 +1,18 @@
-// Build JavaScript Objects
+// Accessing Object Properties with Dot Notation
 
 /*
 
-Make an object that represents a dog called myDog which contains the properties name (a string), legs, tails and friends.
-
-You can set these object properties to whatever values you want, as long as name is a string, legs and tails are numbers, and friends is an array.
+Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.
 
 */
 
-const myDog = {
-  // Only change code below this line
-  name: "Buddy",
-  legs: 4,
-  tails: 0,
-  friends: ["Play", "Bones"],
-
-  // Only change code above this line
+// Setup
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats",
 };
+
+// Only change code below this line
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
