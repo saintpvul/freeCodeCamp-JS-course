64986e5a2e5bab301bae0f401c07c5dfbd3afea0
saintpvul <saintpvul@gmail.com>
2022-07-21 23:43:19 +0600

    feat: complete ex81

--- basic_JS/main.js
+++ basic_JS/main.js
@@ -1,17 +1,23 @@
-// Returning Boolean Values from Functions
+// Return Early Pattern for Functions
 
 /*
 
-Fix the function isLess to remove the if/else statements.
+Modify the function abTest so that if a or b are less than 0 the function will immediately exit with a value of undefined.
 
+Hint
+Remember that undefined is a keyword, not a string.
 */
 
-function isLess(a, b) {
+// Setup
+function abTest(a, b) {
   // Only change code below this line
-
-  return a < b;
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
 
   // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-isLess(10, 15);
+abTest(2, 2);
