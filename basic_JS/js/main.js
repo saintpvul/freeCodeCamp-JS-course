302dce12c302e8ab065550c1c62204321bdedd6a
saintpvul <saintpvul@gmail.com>
2022-07-23 03:14:59 +0600

    feat: complete 93ex

--- basic_JS/js/main.js
+++ basic_JS/js/main.js
@@ -1,23 +1,22 @@
-// Accessing Nested Objects
+// Accessing Nested Arrays
 
 /*
 
-Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. Use dot notation for all properties where possible, otherwise use bracket notation.
+Using dot and bracket notation, set the variable secondTree to the second item in the trees list from the myPlants object.
 
 
 
 */
 
-const myStorage = {
-  car: {
-    inside: {
-      "glove box": "maps",
-      "passenger seat": "crumbs",
-    },
-    outside: {
-      trunk: "jack",
-    },
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
   },
-};
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
 
-const gloveBoxContents = myStorage.car.inside["glove box"];
+const secondTree = myPlants[1].list[1];
