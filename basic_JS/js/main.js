92c8c7d14f5564ea45bb842272c42d5ff3c66824
saintpvul <saintpvul@gmail.com>
2022-07-23 03:47:16 +0600

    feat: complete 96ex

--- basic_JS/js/main.js
+++ basic_JS/js/main.js
@@ -1,16 +1,14 @@
-// Iterate with JavaScript While Loops
+// Iterate with JavaScript For Loops
 
 /*
 
-Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+Use a for loop to push the values 1 through 5 onto myArray.
 
 */
 
 // Setup
 const myArray = [];
-let i = 5;
-// Only change code below this line
-while (i > -1) {
-  myArray.push(i);
-  i--;
+for (let o = 1; o <= 5; o++) {
+  myArray.push(o);
 }
+// Only change code below this line
