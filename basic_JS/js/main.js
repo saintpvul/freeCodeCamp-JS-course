f8bc82141f63827d5c80fe37f65df3d5e50c3200
saintpvul <saintpvul@gmail.com>
2022-07-23 03:07:11 +0600

    feat: complete 91ex

--- basic_JS/js/main.js
+++ basic_JS/js/main.js
@@ -1,18 +1,24 @@
-// Testing Objects for Properties
+// Manipulating Complex Objects
 
 /*
 
-Modify the function checkObj to test if an object passed to the function (obj) contains a specific property (checkProp). If the property is found, return that property's value. If not, return "Not Found".
+Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
 
 */
 
-function checkObj(obj, checkProp) {
-  // Only change code below this line
-  if (obj.hasOwnProperty(checkProp)) {
-    return obj[checkProp];
-  } else {
-    return "Not Found";
-  }
-  // Only change code above this line
-}
-// ! EX 90 COMPLETED // Testing Objects for Properties
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "B.B. King",
+    title: "Thrill is gone",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+];
