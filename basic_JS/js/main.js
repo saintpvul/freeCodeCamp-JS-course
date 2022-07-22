71994de7a402bb041fcd91c45caae9e7ef81ff58
saintpvul <saintpvul@gmail.com>
2022-07-23 03:51:44 +0600

    feat: complete 98ex

--- basic_JS/js/main.js
+++ basic_JS/js/main.js
@@ -1,14 +1,14 @@
-// Iterate Odd Numbers With a For Loop
+// Count Backwards With a For Loop
 
 /*
 
-Push the odd numbers from 1 through 9 to myArray using a for loop.
+Push the odd numbers from 9 through 1 to myArray using a for loop.
 
 */
 
 // Setup
 const myArray = [];
-for (let i = 1; i <= 9; i += 2) {
+for (let i = 9; i >= 0; i -= 2) {
   myArray.push(i);
 }
 // Only change code below this line
