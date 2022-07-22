bde11e9ac0c3ccc1aac3aaacf34c394cbaaad3ee
saintpvul <saintpvul@gmail.com>
2022-07-23 04:29:16 +0600

    feat: complete ex101

--- basic_JS/js/main.js
+++ basic_JS/js/main.js
@@ -1,25 +1,17 @@
-// Nesting For Loops
+// Iterate with JavaScript Do...While Loops
 
 /*
 
-Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.
 
 */
 
-function multiplyAll(arr) {
-  let product = 1;
-  // Only change code below this line
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      product *= arr[i][j];
-    }
-  }
-  // Only change code above this line
-  return product;
-}
+// Setup
+const myArray = [];
+let i = 10;
 
-multiplyAll([
-  [1, 2],
-  [3, 4],
-  [5, 6, 7],
-]);
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
