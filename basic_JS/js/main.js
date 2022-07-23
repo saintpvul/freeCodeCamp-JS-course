206a358e639dc7ff7fde0fd02788f57308f19ddf
saintpvul <saintpvul@gmail.com>
2022-07-23 18:55:05 +0600

    feat: complete ex110

--- basic_JS/js/main.js
+++ basic_JS/js/main.js
@@ -1,12 +1,12 @@
-// Use the Conditional (Ternary) Operator
+// Use Multiple Conditional (Ternary) Operators
 /*
 
-Use the conditional operator in the checkEqual function to check if two numbers are equal or not. The function should return either the string Equal or the string Not Equal.
+In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. The function should return positive, negative or zero.
 
 */
 
-function checkEqual(a, b) {
-  return a == b ? "Equal" : "Not Equal";
+function checkSign(num) {
+  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
 }
 
-checkEqual(1, 2);
+checkSign(10);
