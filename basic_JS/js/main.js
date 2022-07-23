f39b9b6cbe004b0b6d7ef7ce7e4ce430414193d3
saintpvul <saintpvul@gmail.com>
2022-07-23 18:17:08 +0600

    feat: complete ex106

--- basic_JS/js/main.js
+++ basic_JS/js/main.js
@@ -1,13 +1,13 @@
-// Generate Random Whole Numbers with JavaScript
+// Generate Random Whole Numbers within a Range
 
 /*
 
-Use this technique to generate and return a random whole number between 0 and 9.
+Create a function called randomRange that takes a range myMin and myMax and returns a random whole number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
 
 */
 
-function randomWholeNum() {
+function randomRange(myMin, myMax) {
   // Only change code below this line
-
-  return Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  // Only change code above this line
 }
