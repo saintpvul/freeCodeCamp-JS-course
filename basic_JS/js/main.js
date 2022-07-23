42651218f9ed9fed619a5e3c99d9ac767f356a58
saintpvul <saintpvul@gmail.com>
2022-07-23 19:42:08 +0600

    feat: complete ex111

--- basic_JS/js/main.js
+++ basic_JS/js/main.js
@@ -1,12 +1,18 @@
-// Use Multiple Conditional (Ternary) Operators
+// Use Recursion to Create a Countdown
 /*
 
-In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. The function should return positive, negative or zero.
+We have defined a function called countdown with one parameter (n). The function should use recursion to return an array containing the integers n through 1 based on the n parameter. If the function is called with a number less than 1, the function should return an empty array. For example, calling this function with n = 5 should return the array [5, 4, 3, 2, 1]. Your function must use recursion by calling itself and must not use loops of any kind.
 
 */
 
-function checkSign(num) {
-  return num > 0 ? "positive" : num < 0 ? "negative" : "zero";
+// Only change code below this line
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const count = countdown(n - 1);
+    count.unshift(n);
+    return count;
+  }
 }
-
-checkSign(10);
+// Only change code above this line
