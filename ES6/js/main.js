e8052c150af32fbb09a56eaca1e2e1cddb9fbf11
saintpvul <saintpvul@gmail.com>
2022-07-24 19:39:42 +0600

    feat: complete ES6 ex11

--- ES6/js/main.js
+++ ES6/js/main.js
@@ -1,19 +1,21 @@
-//  Use Destructuring Assignment to Assign Variables from Objects
+//  Use Destructuring Assignment to Assign Variables from Nested Objects
 
 /*
 
-Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables highToday and highTomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables lowToday and highToday the values of today.low and today.high from the LOCAL_FORECAST object.
 
 */
 
-const HIGH_TEMPERATURES = {
-  yesterday: 75,
-  today: 77,
-  tomorrow: 80,
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 },
 };
 
 // Only change code below this line
 
-const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+const {
+  today: { low: lowToday, high: highToday },
+} = LOCAL_FORECAST;
 
 // Only change code above this line
