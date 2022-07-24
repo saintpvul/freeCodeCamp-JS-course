77e214fa0fd960e3cf5cb7cb38c734bb9118f547
saintpvul <saintpvul@gmail.com>
2022-07-24 19:21:37 +0600

    feat: complete ES6 ex7

--- ES6/js/main.js
+++ ES6/js/main.js
@@ -1,11 +1,13 @@
-//  Set Default Parameters for Your Functions
+//  Use the Rest Parameter with Function Parameters
 
 /*
 
-Modify the function increment by adding default parameters so that it will add 1 to number if value is not specified.
+Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
+
+
 
 */
 
-// Only change code below this line
-const increment = (number, value = 1) => number + value;
-// Only change code above this line
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+};
