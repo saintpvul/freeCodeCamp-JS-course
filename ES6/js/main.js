d7fb5da156f82c643b983b53a52ad9c4d001f9c4
saintpvul <saintpvul@gmail.com>
2022-07-24 21:48:30 +0600

    feat: complete ES6 ex16

--- ES6/js/main.js
+++ ES6/js/main.js
@@ -1,32 +1,9 @@
-//  Create Strings using Template Literals
+//  Write Concise Object Literal Declarations Using Object Property Shorthand
 
 /*
 
-Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and have a class attribute with the value text-warning. The makeList function should return the array of list item strings.
-
-Use an iterator method (any kind of loop) to get the desired output (shown below).
-[
-  '<li class="text-warning">no-var</li>',
-  '<li class="text-warning">var-on-top</li>',
-  '<li class="text-warning">linebreak</li>'
-]
+Use object property shorthand with object literals to create and return an object with name, age and gender properties.
 
 */
 
-const result = {
-  success: ["max-length", "no-amd", "prefer-arrow-functions"],
-  failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["no-extra-semi", "no-dup-keys"],
-};
-function makeList(arr) {
-  // Only change code below this line
-  const failureItems = [];
-  for (let i = 0; i < arr.length; i++) {
-    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
-  }
-  // Only change code above this line
-
-  return failureItems;
-}
-
-const failuresList = makeList(result.failure);
+const createPerson = (name, age, gender) => ({ name, age, gender });
