b7dd4c59cd3f6103d83dbe3fed941871c19fd669
saintpvul <saintpvul@gmail.com>
2022-07-24 18:35:03 +0600

    feat: complete ES6 ex3

--- ES6/js/main.js
+++ ES6/js/main.js
@@ -1,32 +1,23 @@
-//  Mutate an Array Declared with const
+//  Prevent Object Mutation
 
 /*
 
-An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element assignments.
-
+In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, add, or delete properties.
 
 */
 
-const s0 = [5, 7, 2];
-function editInPlace() {
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
   // Only change code below this line
-  let popS = s0.pop();
-  let unshiftPop = s0.unshift(popS);
-  return s0;
-  // Using s = [2, 5, 7] would be invalid
-
+  Object.freeze(MATH_CONSTANTS);
   // Only change code above this line
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
 }
-editInPlace();
-
-const s1 = [5, 7, 2];
-function editInPlace() {
-  // Only change code below this line
-  s1[0] = 2;
-  s1[1] = 5;
-  s1[2] = 7;
-  // Using s = [2, 5, 7] would be invalid
-
-  // Only change code above this line
-}
-editInPlace();
+const PI = freezeObj();
