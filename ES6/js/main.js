bc43b02b350c46b6d24b34721ecbb2397f64d84b
saintpvul <saintpvul@gmail.com>
2022-07-24 21:45:01 +0600

    feat: complete ES6 ex15

--- ES6/js/main.js
+++ ES6/js/main.js
@@ -1,20 +1,32 @@
-//  Use Destructuring Assignment to Pass an Object as a Function's Parameters
+//  Create Strings using Template Literals
 
 /*
 
-Use destructuring assignment within the argument to the function half to send only max and min inside the function.
+Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and have a class attribute with the value text-warning. The makeList function should return the array of list item strings.
+
+Use an iterator method (any kind of loop) to get the desired output (shown below).
+[
+  '<li class="text-warning">no-var</li>',
+  '<li class="text-warning">var-on-top</li>',
+  '<li class="text-warning">linebreak</li>'
+]
 
 */
 
-const stats = {
-  max: 56.78,
-  standard_deviation: 4.34,
-  median: 34.54,
-  mode: 23.87,
-  min: -0.75,
-  average: 35.85,
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"],
 };
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+  // Only change code above this line
 
-// Only change code below this line
-const half = ({ max, min }) => (min + max) / 2;
-// Only change code above this line
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
