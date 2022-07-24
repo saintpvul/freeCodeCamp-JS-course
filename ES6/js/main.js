6ff721dcb1f84d16895f49961b37f746bd60e741
saintpvul <saintpvul@gmail.com>
2022-07-24 19:03:46 +0600

    feat: complete ES6 ex4

--- ES6/js/main.js
+++ ES6/js/main.js
@@ -1,23 +1,9 @@
-//  Prevent Object Mutation
+//  Use Arrow Functions to Write Concise Anonymous Functions
 
 /*
 
-In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, add, or delete properties.
+Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax. Also, make sure nothing is defined using the keyword var.
 
 */
 
-function freezeObj() {
-  const MATH_CONSTANTS = {
-    PI: 3.14,
-  };
-  // Only change code below this line
-  Object.freeze(MATH_CONSTANTS);
-  // Only change code above this line
-  try {
-    MATH_CONSTANTS.PI = 99;
-  } catch (ex) {
-    console.log(ex);
-  }
-  return MATH_CONSTANTS.PI;
-}
-const PI = freezeObj();
+const magic = () => new Date();
