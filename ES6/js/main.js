35ff691813601af5236fb7e276a18f846447699d
saintpvul <saintpvul@gmail.com>
2022-07-25 22:30:56 +0600

    feat: complete ES6 ex28

--- ES6/js/main.js
+++ ES6/js/main.js
@@ -1,19 +1,19 @@
-//  Complete a Promise with resolve and reject
+//  Handle a Fulfilled Promise with then
 /*
 
-Make the promise handle success and failure. If responseFromServer is true, call the resolve method to successfully complete the promise. Pass resolve a string with the value We got the data. If responseFromServer is false, use the reject method instead and pass it the string: Data not received.
+Add the then method to your promise. Use result as the parameter of its callback function and log result to the console.
 
 */
 
 const makeServerRequest = new Promise((resolve, reject) => {
-  // responseFromServer represents a response from a server
-  let responseFromServer;
-
-  if (responseFromServer) {
-    resolve("We got the data");
-    // Change this line
-  } else {
-    reject("Data not received");
-    // Change this line
-  }
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+  makeServerRequest.then((result) => {
+    if (responseFromServer) {
+      resolve("We got the data");
+    } else {
+      reject("Data not received");
+    }
+  });
+  console.log(result);
 });
