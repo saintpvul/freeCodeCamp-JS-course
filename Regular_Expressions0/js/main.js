5313c0d16964be2bc02cd4ec541a57fb65e4d667
saintpvul <saintpvul@gmail.com>
2022-07-25 22:47:33 +0600

    feat: complete regExp ex1

---
+++ Regular_Expressions0/js/main.js
@@ -0,0 +1,14 @@
+// Using the Test Method
+
+/*
+
+Apply the regex myRegex on the string myString using the .test() method.
+
+*/
+
+// Solution
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+console.log(result);
