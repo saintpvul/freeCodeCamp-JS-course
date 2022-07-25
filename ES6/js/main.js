0a53c3b8d7dc4e69f84aef2bd45d46c95cfff52a
saintpvul <saintpvul@gmail.com>
2022-07-25 22:09:08 +0600

    feat: complete ES6 ex27

--- ES6/js/main.js
+++ ES6/js/main.js
@@ -1,8 +1,19 @@
-//  Create a JavaScript Promise
+//  Complete a Promise with resolve and reject
 /*
 
-Create a new promise called makeServerRequest. Pass in a function with resolve and reject parameters to the constructor.
+Make the promise handle success and failure. If responseFromServer is true, call the resolve method to successfully complete the promise. Pass resolve a string with the value We got the data. If responseFromServer is false, use the reject method instead and pass it the string: Data not received.
 
 */
 
-const makeServerRequest = new Promise((resolve, reject) => {});
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer represents a response from a server
+  let responseFromServer;
+
+  if (responseFromServer) {
+    resolve("We got the data");
+    // Change this line
+  } else {
+    reject("Data not received");
+    // Change this line
+  }
+});
