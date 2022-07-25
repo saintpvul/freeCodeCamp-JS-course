ed5ac3ff58d37b85b1ce862d6d4629825f7727bd
saintpvul <saintpvul@gmail.com>
2022-07-25 23:56:55 +0600

    feat: complete regExp ex8

--- Regular_Expressions0/js/main.js
+++ Regular_Expressions0/js/main.js
@@ -1,16 +1,15 @@
-// Match Single Character with Multiple Possibilities
+// Match Letters of the Alphabet
 
 /*
 
-Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the vowels in the string quoteSample.
+Match all the letters in the string quoteSample.
 
-Note: Be sure to match both upper- and lowercase vowels.
+Note: Be sure to match both uppercase and lowercase letters.
 
 */
 
 // Solution
 
-let quoteSample =
-  "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-let vowelRegex = /[aeiou]/gi; // Change this line
-let result = quoteSample.match(vowelRegex); // Change this line
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
