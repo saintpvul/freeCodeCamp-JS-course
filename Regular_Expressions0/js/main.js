43b648e3bed9b5e7d61b1249f2f52b9f2c346dbc
saintpvul <saintpvul@gmail.com>
2022-07-27 00:53:51 +0600

    feat: complete regExp ex25

--- Regular_Expressions0/js/main.js
+++ Regular_Expressions0/js/main.js
@@ -1,14 +1,14 @@
-// Specify Upper and Lower Number of Matches
+// Specify Only the Lower Number of Matches
 
 /*
 
-Change the regex ohRegex to match the entire phrase Oh no only when it has 3 to 6 letter h's.
+Change the regex haRegex to match the word Hazzah only when it has four or more letter z's.
 
 
 */
 
 // Solution
 
-let ohStr = "Ohhh no";
-let ohRegex = /oh{3,6} no/i; // Change this line
-let result = ohRegex.test(ohStr);
+let haStr = "Hazzzzah";
+let haRegex = /haz{4,}ah/i; // Change this line
+let result = haRegex.test(haStr);
