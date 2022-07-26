ed96f965d8c340a55e8b4f49038e17619c072fe7
saintpvul <saintpvul@gmail.com>
2022-07-27 01:23:59 +0600

    feat: complete regExp ex29

--- Regular_Expressions0/js/main.js
+++ Regular_Expressions0/js/main.js
@@ -1,14 +1,17 @@
-// Positive and Negative Lookahead
+// Check For Mixed Grouping of Characters
 
 /*
 
-Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long, and have two consecutive digits.
+Fix the regex so that it checks for the names of Franklin Roosevelt or Eleanor Roosevelt in a case sensitive manner and it should make concessions for middle names.
+
+Then fix the code so that the regex that you have created is checked against myString and either true or false is returned depending on whether the regex matches.
 
 
 */
 
 // Solution
 
-let sampleWord = "astronaut";
-let pwRegex = /(?=\w{6})(?=\w*\d{2})/g; // Change this line
-let result = pwRegex.test(sampleWord);
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor)(.*) Roosevelt/gi; // Change this line
+let result = myRegex.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
