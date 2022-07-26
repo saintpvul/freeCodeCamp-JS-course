7ec0faee75b39348146f4b3adf1890dd1231c398
saintpvul <saintpvul@gmail.com>
2022-07-27 01:36:20 +0600

    feat: complete regExp ex30

--- Regular_Expressions0/js/main.js
+++ Regular_Expressions0/js/main.js
@@ -1,17 +1,14 @@
-// Check For Mixed Grouping of Characters
+// Reuse Patterns Using Capture Groups
 
 /*
 
-Fix the regex so that it checks for the names of Franklin Roosevelt or Eleanor Roosevelt in a case sensitive manner and it should make concessions for middle names.
-
-Then fix the code so that the regex that you have created is checked against myString and either true or false is returned depending on whether the regex matches.
+Use capture groups in reRegex to match a string that consists of only the same number repeated exactly three times separated by single spaces.
 
 
 */
 
 // Solution
 
-let myString = "Eleanor Roosevelt";
-let myRegex = /(Franklin|Eleanor)(.*) Roosevelt/gi; // Change this line
-let result = myRegex.test(myString); // Change this line
-// After passing the challenge experiment with myString and see how the grouping works
+let repeatNum = "42 42 42";
+let reRegex = /^(\d+) \1 \1$/; // Change this line
+let result = reRegex.test(repeatNum);
