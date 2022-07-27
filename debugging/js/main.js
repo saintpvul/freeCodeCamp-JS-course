7ef1ae016f187e7ad0fab1f6f64d8a3140c65845
saintpvul <saintpvul@gmail.com>
2022-07-28 02:12:47 +0600

    feat: complete debug ex2

--- debugging/js/main.js
+++ debugging/js/main.js
@@ -1,16 +1,16 @@
-//  Use the JavaScript Console to Check the Value of a Variable
+//  Understanding the Differences between the freeCodeCamp and Browser Console
 
 /*
 
-Use the console.log() method to print the value of the variable a where noted in the code.
+First, open your browser console so you can see the logs. To do that, you can right-click the freeCodeCamp navigation bar at the top and click inspect on most browsers. Then find the console tab in the window that opens.
+
+After that, use console.log to log the output variable. View the two consoles to see the log. Finally, use console.clear after your log to clear the browser console. View the difference in the two consoles.
+
+
 
 */
 
-let a = 5;
-let b = 1;
-a++;
-// Only change code below this line
-console.log(a);
-
-let sumAB = a + b;
-console.log(sumAB);
+let output =
+  "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output);
+console.clear();
