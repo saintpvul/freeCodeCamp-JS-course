df507b05344e7d47c5060be4edf5899dafa7efad
saintpvul <saintpvul@gmail.com>
2022-07-28 02:16:13 +0600

    feat: complete debug ex3

--- debugging/js/main.js
+++ debugging/js/main.js
@@ -1,16 +1,16 @@
-//  Understanding the Differences between the freeCodeCamp and Browser Console
+//  Use typeof to Check the Type of a Variable
 
 /*
 
-First, open your browser console so you can see the logs. To do that, you can right-click the freeCodeCamp navigation bar at the top and click inspect on most browsers. Then find the console tab in the window that opens.
-
-After that, use console.log to log the output variable. View the two consoles to see the log. Finally, use console.clear after your log to clear the browser console. View the difference in the two consoles.
+Add two console.log() statements to check the typeof each of the two variables seven and three in the code.
 
 
 
 */
 
-let output =
-  "Get this to show once in the freeCodeCamp console and not at all in the browser console";
-console.log(output);
-console.clear();
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three);
