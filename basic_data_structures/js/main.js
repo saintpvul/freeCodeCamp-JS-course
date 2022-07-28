7133ced30c1467d7adf1225100cf51c2b9dbb312
saintpvul <saintpvul@gmail.com>
2022-07-29 02:55:11 +0600

    feat: complete basic_DS ex13

--- basic_data_structures/js/main.js
+++ basic_data_structures/js/main.js
@@ -1,16 +1,24 @@
-// Create complex multi-dimensional arrays
+// Add Key-Value Pairs to JavaScript Objects
 
 /*
 
-We have defined a variable, myNestedArray, set equal to an array. Modify myNestedArray, using any combination of strings, numbers, and booleans for data elements, so that it has exactly five levels of depth (remember, the outer-most array is level 1). Somewhere on the third level, include the string deep, on the fourth level, include the string deeper, and on the fifth level, include the string deepest.
+A foods object has been created with three entries. Using the syntax of your choice, add three more entries to it: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.
 
 
 */
 
 // Solution
 
-let myNestedArray = [
-  // Only change code below this line
-  [["deep", ["deeper", ["deepest"]]]],
-  // Only change code above this line
-];
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+};
+
+// Only change code below this line
+foods.bananas = 13;
+foods.grapes = 35;
+foods.strawberries = 27;
+// Only change code above this line
+
+console.log(foods);
