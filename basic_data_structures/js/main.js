860072aa014f3c61a5eb6c0a646472a3f7c1c546
saintpvul <saintpvul@gmail.com>
2022-07-29 01:14:23 +0600

    feat: complete basic_DS ex4

--- basic_data_structures/js/main.js
+++ basic_data_structures/js/main.js
@@ -1,20 +1,18 @@
-// Add Items to an Array with push() and unshift()
+// Remove Items from an Array with pop() and shift()
 
 /*
 
-We have defined a function, mixedNumbers, which we are passing an array as an argument. Modify the function by using push() and unshift() to add 'I', 2, 'three' to the beginning of the array and 7, 'VIII', 9 to the end so that the returned array contains representations of the numbers 1-9 in order.
+We have defined a function, popShift, which takes an array as an argument and returns a new array. Modify the function, using pop() and shift(), to remove the first and last elements of the argument array, and assign the removed elements to their corresponding variables, so that the returned array contains their values.
 
 
 */
 
 // Solution
 
-function mixedNumbers(arr) {
-  // Only change code below this line
-  arr.push(7, "VIII", 9);
-  arr.unshift("I", 2, "three");
-  // Only change code above this line
-  return arr;
+function popShift(arr) {
+  let popped = arr.pop(); // Change this line
+  let shifted = arr.shift(); // Change this line
+  return [shifted, popped];
 }
 
-console.log(mixedNumbers(["IV", 5, "six"]));
+console.log(popShift(["challenge", "is", "not", "complete"]));
