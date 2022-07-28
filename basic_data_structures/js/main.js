d22377881daf509542b0961ab8a571edd3bd2827
saintpvul <saintpvul@gmail.com>
2022-07-29 01:24:42 +0600

    feat: complete basic_DS ex6

--- basic_data_structures/js/main.js
+++ basic_data_structures/js/main.js
@@ -1,16 +1,27 @@
-// Remove Items Using splice()
+// Add Items Using splice()
 
 /*
 
-We've initialized an array arr. Use splice() to remove elements from arr, so that it only contains elements that sum to the value of 10.
+We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. Modify the function using splice() to remove the first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in their respective places.
 
 
 */
 
 // Solution
 
-const arr = [2, 4, 5, 1, 7, 5, 2, 1];
-// Only change code below this line
-arr.splice(1, 4);
-// Only change code above this line
-console.log(arr);
+function htmlColorNames(arr) {
+  // Only change code below this line
+  arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+  // Only change code above this line
+  return arr;
+}
+
+console.log(
+  htmlColorNames([
+    "DarkGoldenRod",
+    "WhiteSmoke",
+    "LavenderBlush",
+    "PaleTurquoise",
+    "FireBrick",
+  ])
+);
