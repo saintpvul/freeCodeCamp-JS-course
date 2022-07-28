48b9fb8386265d21d85a7c218ac4fcba511739b4
saintpvul <saintpvul@gmail.com>
2022-07-29 02:22:24 +0600

    feat: complete basic_DS ex11

--- basic_data_structures/js/main.js
+++ basic_data_structures/js/main.js
@@ -1,22 +1,34 @@
-// Check For The Presence of an Element With indexOf()
+// Iterate Through All an Array's Items Using For Loops
 
 /*
 
-indexOf() can be incredibly useful for quickly checking for the presence of an element on an array. We have defined a function, quickCheck, that takes an array and an element as arguments. Modify the function using indexOf() so that it returns true if the passed element exists on the array, and false if it does not.
+We have defined a function, filteredArray, which takes arr, a nested array, and elem as arguments, and returns a new array. elem represents an element that may or may not be present on one or more of the arrays nested within arr. Modify the function, using a for loop, to return a filtered version of the passed array such that any array nested within arr containing elem has been removed.
 
 
 */
 
 // Solution
 
-function quickCheck(arr, elem) {
+function filteredArray(arr, elem) {
+  let newArr = [];
   // Only change code below this line
-  if (arr.indexOf(elem) >= 0) {
-    return true;
-  } else {
-    return false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      newArr.push(arr[i]);
+    }
   }
   // Only change code above this line
+  return newArr;
 }
 
-console.log(quickCheck(["squash", "onions", "shallots"], "mushrooms"));
+console.log(
+  filteredArray(
+    [
+      [3, 2, 3],
+      [1, 6, 3],
+      [3, 13, 26],
+      [19, 3, 9],
+    ],
+    3
+  )
+);
