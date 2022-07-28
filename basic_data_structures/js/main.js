e39272c8e456f79c84ba974342e69ce7d3ab0903
saintpvul <saintpvul@gmail.com>
2022-07-29 02:51:23 +0600

    feat: complete basic_DS ex12

--- basic_data_structures/js/main.js
+++ basic_data_structures/js/main.js
@@ -1,34 +1,16 @@
-// Iterate Through All an Array's Items Using For Loops
+// Create complex multi-dimensional arrays
 
 /*
 
-We have defined a function, filteredArray, which takes arr, a nested array, and elem as arguments, and returns a new array. elem represents an element that may or may not be present on one or more of the arrays nested within arr. Modify the function, using a for loop, to return a filtered version of the passed array such that any array nested within arr containing elem has been removed.
+We have defined a variable, myNestedArray, set equal to an array. Modify myNestedArray, using any combination of strings, numbers, and booleans for data elements, so that it has exactly five levels of depth (remember, the outer-most array is level 1). Somewhere on the third level, include the string deep, on the fourth level, include the string deeper, and on the fifth level, include the string deepest.
 
 
 */
 
 // Solution
 
-function filteredArray(arr, elem) {
-  let newArr = [];
+let myNestedArray = [
   // Only change code below this line
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].indexOf(elem) == -1) {
-      newArr.push(arr[i]);
-    }
-  }
+  [["deep", ["deeper", ["deepest"]]]],
   // Only change code above this line
-  return newArr;
-}
-
-console.log(
-  filteredArray(
-    [
-      [3, 2, 3],
-      [1, 6, 3],
-      [3, 13, 26],
-      [19, 3, 9],
-    ],
-    3
-  )
-);
+];
