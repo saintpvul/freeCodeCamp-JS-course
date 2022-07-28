72bad643cdee36fa90354d0aff01bcc4735495f0
saintpvul <saintpvul@gmail.com>
2022-07-29 03:15:44 +0600

    feat: complete basic_DS ex15

--- basic_data_structures/js/main.js
+++ basic_data_structures/js/main.js
@@ -1,25 +1,27 @@
-// Modify an Object Nested Within an Object
+// Access Property Names with Bracket Notation
 
 /*
 
-Here we've defined an object userActivity, which includes another object nested within it. Set the value of the online key to 45.
+We've defined a function, checkInventory, which receives a scanned item as an argument. Return the current value of the scannedItem key in the foods object. You can assume that only valid keys will be provided as an argument to checkInventory.
 
 
 */
 
 // Solution
 
-let userActivity = {
-  id: 23894201352,
-  date: "January 1, 2017",
-  data: {
-    totalUsers: 51,
-    online: 42,
-  },
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27,
 };
 
-// Only change code below this line
-userActivity.data.online = 45;
-// Only change code above this line
+function checkInventory(scannedItem) {
+  // Only change code below this line
+  return foods[scannedItem];
+  // Only change code above this line
+}
 
-console.log(userActivity);
+console.log(checkInventory("apples"));
