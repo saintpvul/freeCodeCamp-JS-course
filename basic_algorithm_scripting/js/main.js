92662c2d0fa7f9f1310a9cfa9e8549d942d153e8
saintpvul <saintpvul@gmail.com>
2022-07-30 22:27:48 +0600

    feat: complete basic_algoscript ex14

--- basic_algorithm_scripting/js/main.js
+++ basic_algorithm_scripting/js/main.js
@@ -1,22 +1,24 @@
-// Where do I Belong
+// Mutations
 
 /*
 
-Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
 
-For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
 
-Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
 
 */
 
-function getIndexToIns(arr, num) {
-  arr.sort((a, b) => a - b);
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= num) return i;
+function mutation(arr) {
+  let test = arr[1].toLowerCase();
+  let target = arr[0].toLowerCase();
+  for (let i = 0; i < test.length; i++) {
+    if (target.indexOf(test[i]) < 0) return false;
   }
-  return arr.length;
+  return true;
 }
 
-getIndexToIns([40, 60], 50);
+mutation(["hello", "hey"]);
