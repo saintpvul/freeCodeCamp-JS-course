ddcbe4d8bdee09d65ad59305d36ec156a375e9f9
saintpvul <saintpvul@gmail.com>
2022-07-30 22:31:37 +0600

    feat: complete basic_algoscript ex15

--- basic_algorithm_scripting/js/main.js
+++ basic_algorithm_scripting/js/main.js
@@ -1,24 +1,24 @@
-// Mutations
+// Chunky Monkey
 
 /*
 
-Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
-
-For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
-
-The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
-
-Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
 */
 
-function mutation(arr) {
-  let test = arr[1].toLowerCase();
-  let target = arr[0].toLowerCase();
-  for (let i = 0; i < test.length; i++) {
-    if (target.indexOf(test[i]) < 0) return false;
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i % size !== size - 1) newArr.push(arr[i]);
+    else {
+      newArr.push(arr[i]);
+      result.push(newArr);
+      newArr = [];
+    }
   }
-  return true;
-}
 
-mutation(["hello", "hey"]);
+  if (newArr.length !== 0) result.push(newArr);
+  return result;
+}
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
