756dab15cf6af9bf59f273e586c6149a7b6bc6f6
saintpvul <saintpvul@gmail.com>
2022-07-30 21:02:00 +0600

    feat: complete basic_algoscript ex9

--- basic_algorithm_scripting/js/main.js
+++ basic_algorithm_scripting/js/main.js
@@ -1,17 +1,20 @@
-// Truncate a String
+// Finders Keepers
 
 /*
 
-Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
 
 */
 
-function truncateString(str, num) {
-  if (str.length > num) {
-    return str.slice(0, num) + "...";
-  } else {
-    return str;
+function findElement(arr, func) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
   }
+  return undefined;
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 8);
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
