12b89a27aa917f3c34026c95943934a54882ad90
saintpvul <saintpvul@gmail.com>
2022-07-30 22:21:29 +0600

    feat: complete basic_algoscript ex13

--- basic_algorithm_scripting/js/main.js
+++ basic_algorithm_scripting/js/main.js
@@ -1,23 +1,22 @@
-// Falsy Bouncer
+// Where do I Belong
 
 /*
 
-Remove all falsy values from an array.
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
 
-Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
 
-Hint: Try converting each value to a Boolean.
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
 
 */
 
-function bouncer(arr) {
-  let newArr = [];
+function getIndexToIns(arr, num) {
+  arr.sort((a, b) => a - b);
+
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i]) {
-      newArr.push(arr[i]);
-    }
+    if (arr[i] >= num) return i;
   }
-  return newArr;
+  return arr.length;
 }
 
-bouncer([7, "ate", "", false, 9]);
+getIndexToIns([40, 60], 50);
