1b2226cfe8fb2f2d039d8705ed3ab4dd4d6b8264
saintpvul <saintpvul@gmail.com>
2022-07-30 20:08:48 +0600

    feat: complete basic_algoscript ex5

--- basic_algorithm_scripting/js/main.js
+++ basic_algorithm_scripting/js/main.js
@@ -1,22 +1,29 @@
-// Find the Longest Word in a String
+// Return Largest Numbers in Arrays
 
 /*
 
-Return the length of the longest word in the provided sentence.
+Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
 
-Your response should be a number.
+Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
 
 */
 
-function findLongestWordLength(str) {
-  let spliceStr = str.split(" ");
-  let longestWord = 0;
-  for (let i = 0; i < spliceStr.length; i++) {
-    if (spliceStr[i].length > longestWord) {
-      longestWord = spliceStr[i].length;
+function largestOfFour(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let largestNum = arr[i][0];
+    for (let j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNum) {
+        largestNum = arr[i][j];
+      }
     }
+    result[i] = largestNum;
   }
-  return longestWord;
+  return result;
 }
-
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
