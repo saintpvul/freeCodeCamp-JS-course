fb6be9c3b7b17501741e361fc6b7f7bb02615e54
saintpvul <saintpvul@gmail.com>
2022-07-31 22:12:08 +0600

    feat: complete OOP ex6

--- OOP/js/main.js
+++ OOP/js/main.js
@@ -1,16 +1,16 @@
-// Define a Constructor Function
+// Use a Constructor to Create Objects
 
 /*
 
-Create a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
+Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound.
 
 
 */
 
 function Dog() {
-  this.name = "Alfredo";
-  this.color = "grey";
+  this.name = "Rupert";
+  this.color = "brown";
   this.numLegs = 4;
 }
-
-dog.sayLegs();
+// Only change code below this line
+var hound = new Dog();
