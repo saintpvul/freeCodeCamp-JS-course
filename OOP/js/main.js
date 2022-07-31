368f2c34340f8e2d9062bdae3c3f96a430c3d2ce
saintpvul <saintpvul@gmail.com>
2022-07-31 22:20:02 +0600

    feat: complete OOP ex8

--- OOP/js/main.js
+++ OOP/js/main.js
@@ -1,16 +1,16 @@
-// Extend Constructors to Receive Arguments
+// Verify an Object's Constructor with instanceof
 
 /*
 
-Create another Dog constructor. This time, set it up to take the parameters name and color, and have the property numLegs fixed at 4. Then create a new Dog saved in a variable terrier. Pass it two strings as arguments for the name and color properties.
+Create a new instance of the House constructor, calling it myHouse and passing a number of bedrooms. Then, use instanceof to verify that it is an instance of House.
 
 
 */
 
-function Dog(name, color) {
-  this.name = name;
-  this.color = color;
-  this.numLegs = 4;
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
 }
 
-var terrier = new Dog("Austin", "brown");
+// Only change code below this line
+var myHouse = new House(4);
+myHouse instanceof House;
