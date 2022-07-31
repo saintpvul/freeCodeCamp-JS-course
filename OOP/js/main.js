3c137c539987ac5ab2b3572f2cbc374f5b932409
saintpvul <saintpvul@gmail.com>
2022-07-31 22:16:24 +0600

    feat: complete OOP ex7

--- OOP/js/main.js
+++ OOP/js/main.js
@@ -1,16 +1,16 @@
-// Use a Constructor to Create Objects
+// Extend Constructors to Receive Arguments
 
 /*
 
-Use the Dog constructor from the last lesson to create a new instance of Dog, assigning it to a variable hound.
+Create another Dog constructor. This time, set it up to take the parameters name and color, and have the property numLegs fixed at 4. Then create a new Dog saved in a variable terrier. Pass it two strings as arguments for the name and color properties.
 
 
 */
 
-function Dog() {
-  this.name = "Rupert";
-  this.color = "brown";
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
   this.numLegs = 4;
 }
-// Only change code below this line
-var hound = new Dog();
+
+var terrier = new Dog("Austin", "brown");
