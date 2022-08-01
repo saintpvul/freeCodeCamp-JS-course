af4ca5bdd6a94c0c06caa5e1771349707bd8a782
saintpvul <saintpvul@gmail.com>
2022-08-01 22:23:54 +0600

    feat: complete OOP ex21

--- OOP/js/main.js
+++ OOP/js/main.js
@@ -1,24 +1,32 @@
-// Reset an Inherited Constructor Property
-
+// Add Methods After Inheritance
 /*
 
-Fix the code so duck.constructor and beagle.constructor return their respective constructors.
+Add all necessary code so the Dog object inherits from Animal and the Dog's prototype constructor is set to Dog. Then add a bark() method to the Dog object so that beagle can both eat() and bark(). The bark() method should print Woof! to the console.
 
 */
 
 // Solution
 
 function Animal() {}
-function Bird() {}
+Animal.prototype.eat = function () {
+  console.log("nom nom nom");
+};
+
 function Dog() {}
 
-Bird.prototype = Object.create(Animal.prototype);
-Dog.prototype = Object.create(Animal.prototype);
-
 // Only change code below this line
-
-Bird.prototype.constructor = Bird;
+Dog.prototype = Object.create(Animal.prototype);
 Dog.prototype.constructor = Dog;
 
-let duck = new Bird();
+Dog.prototype.bark = function () {
+  console.log("Woof!");
+};
+Dog.prototype.eat = function () {
+  console.log("nom nom nom");
+};
+
+// Only change code above this line
+
 let beagle = new Dog();
+beagle.bark();
+beagle.eat();
