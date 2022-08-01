ba68d3b5cb178d876922b7c47bf45e6a43a5e527
saintpvul <saintpvul@gmail.com>
2022-08-01 23:36:08 +0600

    feat: complete funcProg ex2

--- functioanal_programming/js/main.js
+++ functioanal_programming/js/main.js
@@ -1,24 +1,27 @@
-//  Learn About Functional Programming
+//  Understand Functional Programming Terminology
 
 /*
 
-The members of freeCodeCamp happen to love tea.
+Prepare 27 cups of green tea and 13 cups of black tea and store them in tea4GreenTeamFCC and tea4BlackTeamFCC variables, respectively. Note that the getTea function has been modified so it now takes a function as the first argument.
 
-In the code editor, the prepareTea and getTea functions are already defined for you. Call the getTea function to get 40 cups of tea for the team, and store them in the tea4TeamFCC variable.
+Note: The data (the number of cups of tea) is supplied as the last argument. We'll discuss this more in later lessons.
 
 */
 
 // Solution
 
 // Function that returns a string representing a cup of green tea
-const prepareTea = () => "greenTea";
+const prepareGreenTea = () => "greenTea";
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => "blackTea";
 
 /*
 Given a function (representing the tea type) and number of cups needed, the
 following function returns an array of strings (each representing a cup of
 a specific type of tea).
 */
-const getTea = (numOfCups) => {
+const getTea = (prepareTea, numOfCups) => {
   const teaCups = [];
 
   for (let cups = 1; cups <= numOfCups; cups += 1) {
@@ -29,5 +32,8 @@ const getTea = (numOfCups) => {
 };
 
 // Only change code below this line
-const tea4TeamFCC = getTea(40);
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
 // Only change code above this line
+
+console.log(tea4GreenTeamFCC, tea4BlackTeamFCC);
