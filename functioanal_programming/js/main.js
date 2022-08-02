c8709bc86301638aa06db8b5f461945371191fce
saintpvul <saintpvul@gmail.com>
2022-08-02 19:49:47 +0600

    feat: complete funcProg ex12

--- functioanal_programming/js/main.js
+++ functioanal_programming/js/main.js
@@ -1,20 +1,21 @@
-//  Return Part of an Array Using the slice Method
+//  Remove Elements from an Array Using slice Instead of splice
 
 /*
 
-Use the slice method in the sliceArray function to return part of the anim array given the provided beginSlice and endSlice indices. The function should return an array.
+Rewrite the function nonMutatingSplice by using slice instead of splice. It should limit the provided cities array to a length of 3, and return a new array with only the first three items.
+
+Do not mutate the original array provided to the function.
 
 */
 
 // Solution
 
-function sliceArray(anim, beginSlice, endSlice) {
+function nonMutatingSplice(cities) {
   // Only change code below this line
-
-  return anim.slice(beginSlice, endSlice);
+  return cities.slice(0, 3);
 
   // Only change code above this line
 }
 
-const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
-sliceArray(inputAnim, 1, 3);
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
