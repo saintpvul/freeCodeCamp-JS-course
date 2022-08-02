74240425b591c24878e8d7bd3c5fc0705d959e2a
saintpvul <saintpvul@gmail.com>
2022-08-02 19:52:01 +0600

    feat: complete funcProg ex13

--- functioanal_programming/js/main.js
+++ functioanal_programming/js/main.js
@@ -1,21 +1,20 @@
-//  Remove Elements from an Array Using slice Instead of splice
+//  Combine Two Arrays Using the concat Method
 
 /*
 
-Rewrite the function nonMutatingSplice by using slice instead of splice. It should limit the provided cities array to a length of 3, and return a new array with only the first three items.
-
-Do not mutate the original array provided to the function.
+Use the concat method in the nonMutatingConcat function to concatenate attach to the end of original. The function should return the concatenated array.
 
 */
 
 // Solution
 
-function nonMutatingSplice(cities) {
+function nonMutatingConcat(original, attach) {
   // Only change code below this line
-  return cities.slice(0, 3);
+  return original.concat(attach);
 
   // Only change code above this line
 }
 
-const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
-nonMutatingSplice(inputCities);
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingConcat(first, second);
