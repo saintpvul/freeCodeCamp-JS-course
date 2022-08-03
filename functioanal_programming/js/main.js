3be7f80fa0459d60914ddfba427dc6c5047ca1dc
saintpvul <saintpvul@gmail.com>
2022-08-03 21:28:19 +0600

    feat: complete funcProg ex21

--- functioanal_programming/js/main.js
+++ functioanal_programming/js/main.js
@@ -1,18 +1,28 @@
-//  Combine an Array into a String Using the join Method
+//  Apply Functional Programming to Convert Strings to URL Slugs
 
 /*
 
-Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. The function should return a string. For example, I-like-Star-Wars would be converted to I like Star Wars. For this challenge, do not use the replace method.
+Fill in the urlSlug function so it converts a string title and returns the hyphenated version for the URL. You can use any of the methods covered in this section, and don't use replace. Here are the requirements:
+
+The input is a string with spaces and title-cased words
+
+The output is a string with the spaces between words replaced by a hyphen (-)
+
+The output should be all lower-cased letters
+
+The output should not have any spaces
 
 */
 
 // Solution
 
-function sentensify(str) {
-  // Only change code below this line
-  return str.split(/\W/).join(" ");
-
-  // Only change code above this line
+// Only change code below this line
+function urlSlug(title) {
+  return title
+    .toLowerCase()
+    .split(" ")
+    .filter((substr) => substr !== "")
+    .join("-");
 }
-
-sentensify("May-the-force-be-with-you");
+// Only change code above this line
+urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
