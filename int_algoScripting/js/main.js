2cdfd616d7011b60c0a50e49666a8fee556f6c72
saintpvul <saintpvul@gmail.com>
2022-08-03 23:57:09 +0600

    feat: complete int_algoScript ex7

--- int_algoScripting/js/main.js
+++ int_algoScripting/js/main.js
@@ -1,19 +1,30 @@
-// Pig Latin
+// Search and Replace
 
 /*
 
-Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
 
 */
 
 // Solution
 
-function translatePigLatin(str) {
-  let consonantRegex = /^[^aeoui]+/;
-  let matchConsonant = str.match(consonantRegex);
-  return matchConsonant !== null
-    ? str.replace(consonantRegex, "").concat(matchConsonant).concat("ay")
-    : str.concat("way");
+function myReplace(str, before, after) {
+  let index = str.indexOf(before);
+  if (str[index] === str[index].toUpperCase()) {
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+    after = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+  str = str.replace(before, after);
+  return str;
 }
 
-translatePigLatin("consonant");
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
