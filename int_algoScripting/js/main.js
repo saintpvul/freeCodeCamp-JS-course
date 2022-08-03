248019a643d4513b8caabe70e2d6e3700865dd1b
saintpvul <saintpvul@gmail.com>
2022-08-04 02:12:34 +0600

    feat: complete int_algoScript ex10

--- int_algoScripting/js/main.js
+++ int_algoScripting/js/main.js
@@ -1,25 +1,26 @@
-// Missing letters
+// Sorted Union
 
 /*
 
-Find the missing letter in the passed letter range and return it.
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
 
-If all letters are present in the range, return undefined.
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
 
 */
 
 // Solution
 
-function fearNotLetter(str) {
-  let alphabet = "abcdefghijklmnopqrstuvwxyz";
-  let startingPoing = alphabet.indexOf(str[0]);
-
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] !== alphabet[startingPoing + i]) {
-      return alphabet[startingPoing + i];
-    }
+function uniteUnique(arr) {
+  const newArr = [];
+  for (let i = 0; i < arguments.length; i++) {
+    newArr.push(arguments[i]);
   }
-  return undefined;
+  let flatArr = newArr.flat();
+  return [...new Set(flatArr)];
 }
 
-fearNotLetter("abce");
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
