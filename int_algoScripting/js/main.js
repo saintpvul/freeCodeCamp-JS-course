7f469860eb37deca7f30a271c5d74a242ca29a75
saintpvul <saintpvul@gmail.com>
2022-08-04 00:06:33 +0600

    feat: complete int_algoScript ex8

--- int_algoScripting/js/main.js
+++ int_algoScripting/js/main.js
@@ -1,30 +1,40 @@
-// Search and Replace
+// DNA Pairing
 
 /*
 
-Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
 
-First argument is the sentence to perform the search and replace on.
+The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
 
-Second argument is the word that you will be replacing (before).
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
 
-Third argument is what you will be replacing the second argument with (after).
-
-Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
 
 */
 
 // Solution
 
-function myReplace(str, before, after) {
-  let index = str.indexOf(before);
-  if (str[index] === str[index].toUpperCase()) {
-    after = after.charAt(0).toUpperCase() + after.slice(1);
-  } else {
-    after = after.charAt(0).toLowerCase() + after.slice(1);
+function pairElement(str) {
+  let matchPairs = function (char, pairedArray) {
+    switch (char) {
+      case "A":
+        pairedArray.push(["A", "T"]);
+        break;
+      case "T":
+        pairedArray.push(["T", "A"]);
+        break;
+      case "C":
+        pairedArray.push(["C", "G"]);
+        break;
+      case "G":
+        pairedArray.push(["G", "C"]);
+        break;
+    }
+  };
+  const paired = [];
+  for (let i = 0; i < str.length; i++) {
+    matchPairs(str[i], paired);
   }
-  str = str.replace(before, after);
-  return str;
+  return paired;
 }
-
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+pairElement("GCG");
