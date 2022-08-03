3081869c3d4fd51d455c7286bf5b6de9e6141abb
saintpvul <saintpvul@gmail.com>
2022-08-04 03:01:25 +0600

    feat: complete int_algoScript ex11

--- int_algoScripting/js/main.js
+++ int_algoScripting/js/main.js
@@ -1,26 +1,29 @@
-// Sorted Union
+// Convert HTML Entities
 
 /*
 
-Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
-
-In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
-
-The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
-
-Check the assertion tests for examples.
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 
 */
 
 // Solution
 
-function uniteUnique(arr) {
-  const newArr = [];
-  for (let i = 0; i < arguments.length; i++) {
-    newArr.push(arguments[i]);
-  }
-  let flatArr = newArr.flat();
-  return [...new Set(flatArr)];
+function convertHTML(str) {
+  let regex = /[<|>|&|"|']/g;
+  let replaceStr = str.replace(regex, function (match) {
+    if (match === "<") {
+      return "&lt;";
+    } else if (match === ">") {
+      return "&gt;";
+    } else if (match === "&") {
+      return "&amp;";
+    } else if (match === '"') {
+      return "&quot;";
+    } else if (match === "'") {
+      return "&apos;";
+    }
+  });
+  return replaceStr;
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+convertHTML("Dolce & Gabbana");
