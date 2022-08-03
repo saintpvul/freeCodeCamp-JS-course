37df320f7ce7ab6a481c4c1853964d585ff2bed1
saintpvul <saintpvul@gmail.com>
2022-08-04 03:07:08 +0600

    feat: complete int_algoScript ex12

--- int_algoScripting/js/main.js
+++ int_algoScripting/js/main.js
@@ -1,29 +1,30 @@
-// Convert HTML Entities
+// Sum All Odd Fibonacci Numbers
 
 /*
 
-Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers. The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
 
 */
 
 // Solution
 
-function convertHTML(str) {
-  let regex = /[<|>|&|"|']/g;
-  let replaceStr = str.replace(regex, function (match) {
-    if (match === "<") {
-      return "&lt;";
-    } else if (match === ">") {
-      return "&gt;";
-    } else if (match === "&") {
-      return "&amp;";
-    } else if (match === '"') {
-      return "&quot;";
-    } else if (match === "'") {
-      return "&apos;";
-    }
-  });
-  return replaceStr;
+function sumFibs(num) {
+  let newArr = [1, 1];
+
+  for (let i = 2; i < num; i++) {
+    newArr.push(newArr[i - 2] + newArr[i - 1]);
+  }
+
+  let shortArr = newArr.filter((x) => x <= num);
+  let finalArr = shortArr.filter((x) => x % 2 === 1);
+
+  let sum = finalArr.reduce((a, b) => a + b, 0);
+
+  return sum;
 }
 
-convertHTML("Dolce & Gabbana");
+sumFibs(4);
