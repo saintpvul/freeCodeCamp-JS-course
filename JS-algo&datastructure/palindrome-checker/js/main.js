6e1df06ed6771e148fe0841793702b45dfe400d1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-12 22:06:11 +0600

    init: start roman numeral converter problem

--- JS-algo&datastructure/palindrome-checker/js/main.js
+++ JS-algo&datastructure/palindrome-checker/js/main.js
@@ -1,24 +1,31 @@
-// Palindrome Checker
+// Roman Numeral Converter
 
 /*
 
-Return true if the given string is a palindrome. Otherwise, return false.
+Convert the given number into a roman numeral.
 
-A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+Roman numerals	Arabic numerals
 
-Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+M							1000
+CM						900
+D							500
+CD						400
+C							100
+XC						90
+L							50
+XL						40
+X							10
+IX						9
+V							5
+IV						4
+I							1
 
-We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
-
-We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
+All roman numerals answers should be provided in upper-case.
 
 */
 
-function palindrome(str) {
-  let getDigits = str.toLowerCase().match(/[a-z0-9]/g);
-  if (getDigits.join("") === getDigits.reverse().join("")) {
-    return true;
-  } else {
-    return false;
+function convertToRoman(num) {
+  if (isNan(num)) {
+    return Nan;
   }
 }
