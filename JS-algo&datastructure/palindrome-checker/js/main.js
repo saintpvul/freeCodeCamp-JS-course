1ca8a0b33d6187832ed3ab7ac9956ffb0f73ffbb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-02 21:14:31 +0600

    feat: complete palindrome checker at freecodecamp

--- JS-algo&datastructure/palindrome-checker/js/main.js
+++ JS-algo&datastructure/palindrome-checker/js/main.js
@@ -2,30 +2,24 @@
 
 /*
 
-Convert the given number into a roman numeral.
+Palindrome Checker
+Return true if the given string is a palindrome. Otherwise, return false.
 
-Roman numerals	Arabic numerals
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
 
-M							1000
-CM						900
-D							500
-CD						400
-C							100
-XC						90
-L							50
-XL						40
-X							10
-IX						9
-V							5
-IV						4
-I							1
+Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
 
-All roman numerals answers should be provided in upper-case.
+We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
+
+We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.
+
+
 
 */
 
-function convertToRoman(num) {
-  if (isNan(num)) {
-    return Nan;
-  }
+function palindrome(str) {
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return str === str.split("").reverse().join("");
 }
+
+console.log(palindrome("_eye"));
